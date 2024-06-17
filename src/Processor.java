08da8a31f2a095797e15680867779521b9336bb5
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-06-18 02:12:53 +0300

    Add processor implementation

--- src/Processor.java
+++ src/Processor.java
@@ -1,2 +1,38 @@
 public class Processor {
+    private final String id;
+    private Task currentTask = null;
+    private static int currentId = 1;
+
+    public Processor() {
+        this.id = "P" + currentId;
+        currentId++;
+    }
+
+    public boolean isOccupied(){
+        return currentTask != null;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCurrentTaskId() {
+        return currentTask.getId();
+    }
+
+    public void serve(){
+        if(this.currentTask == null) return;
+
+        this.currentTask.getServed();
+        if(this.currentTask.isFinished()){
+            currentTask = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Processor id= " + id + " {" +
+                ", currentTask= " + getCurrentTaskId() +
+                '}';
+    }
 }
