438f4af9275c5aeef0ac71f019aac45bc9f094d7
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-07-01 01:58:08 +0300

    Add Clock Module

---
+++ src/Clock.java
@@ -0,0 +1,23 @@
+public class Clock {
+    private static Clock clock;
+    private int currentCycle;
+
+    private Clock(){
+        currentCycle = 1;
+    }
+
+    public static Clock getInstance(){
+        if(clock == null){
+            clock = new Clock();
+        }
+        return clock;
+    }
+
+    public String getCurrentCycle() {
+        return "C" + currentCycle;
+    }
+
+    public void nextCycle(){
+        currentCycle++;
+    }
+}
