084ab06d52b2d15889211d7151692d617f952432
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-07-02 20:01:06 +0300

    Add scheduler

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,39 +1,59 @@
 import java.util.*;
+
 public class Scheduler {
     private PriorityQueue<Task> highPriorityTasks;
     private PriorityQueue<Task> lowPriorityTasks;
-    private Stack<Processor> idleProcessors;
+    private ArrayList<Processor> idleProcessors;
+    private ArrayList<Processor> busyProcessors;
 
-    public Scheduler(){
+    public Scheduler() {
         highPriorityTasks = new PriorityQueue<Task>();
         lowPriorityTasks = new PriorityQueue<Task>();
-        idleProcessors = new Stack<Processor>();
+        idleProcessors = new ArrayList<Processor>();
+        busyProcessors = new ArrayList<Processor>();
     }
 
-    public void addTaskToQueue(Task task){
-        if(task == null) throw new IllegalArgumentException();
-        if(task.getPriority() == 1){
+    public void addTaskToQueue(Task task) {
+        if (task == null) throw new IllegalArgumentException();
+        if (task.getPriority() == 1) {
             highPriorityTasks.add(task);
-        }
-        else{
+        } else {
             lowPriorityTasks.add(task);
         }
     }
 
-    public void scheduleTask(){
-        if(idleProcessors.isEmpty()) return;
+    public void scheduleTask() {
+        System.out.println(lowPriorityTasks.size() + highPriorityTasks.size());
+        if (idleProcessors.isEmpty()) return;
+        while(!idleProcessors.isEmpty() && !highPriorityTasks.isEmpty() && !lowPriorityTasks.isEmpty()){
+            Processor tmp = idleProcessors.remove(idleProcessors.size()-1);
+            if (highPriorityTasks.isEmpty()) {
+                tmp.assignTask(lowPriorityTasks.poll());
+            } else {
+                tmp.assignTask(highPriorityTasks.poll());
+            }
 
-        if(highPriorityTasks.isEmpty()){
-            idleProcessors.pop().assignTask(lowPriorityTasks.poll());
-        }
-        else{
-            idleProcessors.pop().assignTask(highPriorityTasks.poll());
+            busyProcessors.add(tmp);
         }
     }
 
-    public void addIdleProcessor(Processor processor){
-        if(processor == null) throw new IllegalArgumentException();
-        idleProcessors.push(processor);
+    public void addIdleProcessor(Processor processor) {
+        if (processor == null) throw new IllegalArgumentException();
+        idleProcessors.add(processor);
     }
 
+    public void sweep(){
+        for(int i=busyProcessors.size()-1;i >=0;i--){
+            if(!busyProcessors.get(i).isOccupied()){
+                idleProcessors.add(busyProcessors.get(i));
+                busyProcessors.remove(i);
+            }
+        }
+        for(int i=idleProcessors.size()-1;i >=0;i--){
+            if(idleProcessors.get(i).isOccupied()){
+                busyProcessors.add(idleProcessors.get(i));
+                idleProcessors.remove(i);
+            }
+        }
+    }
 }
